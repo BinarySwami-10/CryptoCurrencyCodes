cfa682c0c81a62764deccba1251ce01b79d17562
Nikhil-Swami <54004431+MasterMindNikhilSwami@users.noreply.github.com>
2020-09-16 07:56:08 +0530

    updates

--- CryptoStockPriceGet.py
+++ CryptoStockPriceGet.py
@@ -7,27 +7,20 @@ import os
 import time as t
 
 url='https://bitcointicker.co/'
+opts=selenium.webdriver.firefox.options.Options()
 driver = webdriver.Firefox()
-driver.get(url)
+# driver.get(url)
 temp_val=[0]
-for i in range(0,100):
-	t.sleep(1)
+# for i in range(0,100):
+# 	t.sleep(1)
+# 	heading1 = driver.find_element_by_id('lastTrade').text
+# 	temp_val.append(heading1)
+# 	if temp_val[1]==temp_val[0]:
+# 		pass
+# 	else:
+# 		print(temp_val[1])
+# 	temp_val.pop(0) #because We using FIFO data structure
 
-	heading1 = driver.find_element_by_id('lastTrade').text
-	temp_val.append(heading1)
-	if temp_val[1]==temp_val[0]:
-		# print('same')
-		pass
-	else:
-		print(temp_val[1])
-	temp_val.pop(0)
-
-driver.close()
+# driver.close()
 
 
-
-
-
-# dirs=os.sys.path
-# dirs.append('/home/nikhil/Desktop/python')
-# print(dirs)
